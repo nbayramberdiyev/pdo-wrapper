8f638f4573d78c64d9cc221ba5bb6faac7189e83
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2019-09-10 23:38:30 +0300

    Implement PSR-12

--- src/Database.php
+++ src/Database.php
@@ -1,46 +1,50 @@
 <?php
-    declare(strict_types=1);
 
-    namespace NB;
+declare(strict_types=1);
 
-    use PDO;
-    use PDOStatement;
+namespace NB;
 
-    class Database extends PDO {
-        /**
-         * Create a new Database instance.
-         *
-         * @param string $dsn
-         * @param string $username
-         * @param string $password
-         * @param array $options
-         * @return void
-         */
-        public function __construct(string $dsn, string $username, string $password, array $options = []) {
-            $default_options = [
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            ];
+use PDO;
+use PDOStatement;
 
-            $options = array_replace($default_options, $options);
+class Database extends PDO
+{
+    /**
+     * Create a new Database instance.
+     *
+     * @param string $dsn
+     * @param string $username
+     * @param string $password
+     * @param array $options
+     * @return void
+     */
+    public function __construct(string $dsn, string $username, string $password, array $options = [])
+    {
+        $default_options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
 
-            parent::__construct($dsn, $username, $password, $options);
-        }
+        $options = array_replace($default_options, $options);
 
-        /**
-         * Shorthand for prepare() and execute() methods.
-         *
-         * @param string $sql
-         * @param array $args
-         * @return PDOStatement
-         */
-        public function run(string $sql, array $args = []): PDOStatement {
-            if (!$args) {
-                return $this->query($sql);
-            }
-            $stmt = $this->prepare($sql);
-            $stmt->execute($args);
-            return $stmt;
-        }
+        parent::__construct($dsn, $username, $password, $options);
     }
+
+    /**
+     * Shorthand for prepare() and execute() methods.
+     *
+     * @param string $sql
+     * @param array $args
+     * @return PDOStatement
+     */
+    public function run(string $sql, array $args = []): PDOStatement
+    {
+        if (!$args) {
+            return $this->query($sql);
+        }
+        $stmt = $this->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+}
