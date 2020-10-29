b0153c196b8e90f1bc93737971b9f9d5ac53c695
N'Bayramberdiyev <nbayramberdiyev@gmail.com>
2020-10-29 12:28:21 +0300

    Add spaces and blank lines

--- src/Database.php
+++ src/Database.php
@@ -15,18 +15,18 @@ class Database extends PDO
      * @param string $dsn
      * @param string $username
      * @param string $password
-     * @param array $options
+     * @param array  $options
      * @return void
      */
     public function __construct(string $dsn, string $username, string $password, array $options = [])
     {
-        $default_options = [
+        $defaultOptions = [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         ];
 
-        $options = array_replace($default_options, $options);
+        $options = array_replace($defaultOptions, $options);
 
         parent::__construct($dsn, $username, $password, $options);
     }
@@ -43,8 +43,10 @@ class Database extends PDO
         if (!$args) {
             return $this->query($sql);
         }
+
         $stmt = $this->prepare($sql);
         $stmt->execute($args);
+
         return $stmt;
     }
 }
